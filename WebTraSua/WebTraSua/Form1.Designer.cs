7bc2230f9db60b40b083582f07e2357831654e95
HuynhBaoLong96 <05baolong@gmail.com>
2025-06-22 23:04:27 +0700

    Chinh sua gio hang

---
+++ WebTraSua/WebTraSua/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WebTraSua
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_gh = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_gh
+            // 
+            this.lbl_gh.AutoSize = true;
+            this.lbl_gh.Location = new System.Drawing.Point(356, 78);
+            this.lbl_gh.Name = "lbl_gh";
+            this.lbl_gh.Size = new System.Drawing.Size(50, 13);
+            this.lbl_gh.TabIndex = 0;
+            this.lbl_gh.Text = "Giỏ hàng";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_gh);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_gh;
+    }
+}
+
