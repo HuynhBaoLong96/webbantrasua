a4af22230bc3356d6a294b18ec48e448f43461ce
thienitcoder2004 <thienitcoder2004@gmail.com>
2025-06-23 23:14:34 +0700

    thêm tìm kiếm và xuất hóa đơn

--- WebTraSua/WebTraSua/Form1.Designer.cs
+++ WebTraSua/WebTraSua/Form1.Designer.cs
@@ -39,24 +39,24 @@
             this.label4 = new System.Windows.Forms.Label();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // lbl_gh
             // 
             this.lbl_gh.AutoSize = true;
-            this.lbl_gh.Location = new System.Drawing.Point(213, 89);
+            this.lbl_gh.Location = new System.Drawing.Point(189, 121);
             this.lbl_gh.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_gh.Name = "lbl_gh";
-            this.lbl_gh.Size = new System.Drawing.Size(74, 20);
+            this.lbl_gh.Size = new System.Drawing.Size(61, 16);
             this.lbl_gh.TabIndex = 0;
             this.lbl_gh.Text = "Giỏ hàng";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(156, 374);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Location = new System.Drawing.Point(141, 333);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(192, 29);
+            this.button1.Size = new System.Drawing.Size(171, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "Them vao gio";
             this.button1.UseVisualStyleBackColor = true;
@@ -64,10 +64,9 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(156, 292);
-            this.checkBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.checkBox1.Location = new System.Drawing.Point(119, 190);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(73, 24);
+            this.checkBox1.Size = new System.Drawing.Size(60, 20);
             this.checkBox1.TabIndex = 2;
             this.checkBox1.Text = "Chọn";
             this.checkBox1.UseVisualStyleBackColor = true;
@@ -75,19 +74,19 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(130, 199);
+            this.label1.Location = new System.Drawing.Point(116, 159);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.Size = new System.Drawing.Size(51, 16);
             this.label1.TabIndex = 3;
             this.label1.Text = "Tên Sp";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(867, 70);
+            this.button2.Location = new System.Drawing.Point(771, 56);
             this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(222, 59);
+            this.button2.Size = new System.Drawing.Size(197, 47);
             this.button2.TabIndex = 4;
             this.button2.Text = "Đăng xuất";
             this.button2.UseVisualStyleBackColor = true;
@@ -95,10 +94,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(943, 385);
+            this.button3.Location = new System.Drawing.Point(838, 374);
             this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(146, 59);
+            this.button3.Size = new System.Drawing.Size(130, 47);
             this.button3.TabIndex = 5;
             this.button3.Text = "Thanh toán";
             this.button3.UseVisualStyleBackColor = true;
@@ -107,54 +106,63 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(993, 350);
+            this.label2.Location = new System.Drawing.Point(883, 333);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(100, 20);
+            this.label2.Size = new System.Drawing.Size(85, 16);
             this.label2.TabIndex = 6;
             this.label2.Text = "Tổng giá tiền";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(479, 109);
+            this.label3.Location = new System.Drawing.Point(426, 43);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(211, 20);
+            this.label3.Size = new System.Drawing.Size(172, 16);
             this.label3.TabIndex = 7;
             this.label3.Text = "Quan li thong tin khách hang";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(479, 216);
+            this.label4.Location = new System.Drawing.Point(456, 290);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(217, 20);
+            this.label4.Size = new System.Drawing.Size(184, 16);
             this.label4.TabIndex = 8;
             this.label4.Text = "Thống kê sản phẩm bán chạy";
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(529, 269);
-            this.button4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button4.Location = new System.Drawing.Point(501, 330);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(84, 29);
+            this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 9;
             this.button4.Text = "Thong ke";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(851, 164);
+            this.button5.Location = new System.Drawing.Point(771, 131);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(265, 55);
+            this.button5.Size = new System.Drawing.Size(197, 44);
             this.button5.TabIndex = 10;
             this.button5.Text = "Xem nhật ký hoạt động";
             this.button5.UseVisualStyleBackColor = true;
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(119, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(242, 22);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.Text = "Tìm kiếm";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.label4);
@@ -166,7 +174,7 @@
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.lbl_gh);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -187,6 +195,7 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
