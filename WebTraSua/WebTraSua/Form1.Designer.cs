9789e57199952e4c2209cd72da40df0027e85c75
TuThanh11 <Caottu1980@gmail.com>
2025-06-23 11:02:27 +0700

    Cao Nguyen Thanh Tu phan quyen cho cac vai tro

--- WebTraSua/WebTraSua/Form1.Designer.cs
+++ WebTraSua/WebTraSua/Form1.Designer.cs
@@ -31,23 +31,25 @@
             this.lbl_gh = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lbl_gh
             // 
             this.lbl_gh.AutoSize = true;
-            this.lbl_gh.Location = new System.Drawing.Point(189, 71);
+            this.lbl_gh.Location = new System.Drawing.Point(213, 89);
             this.lbl_gh.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_gh.Name = "lbl_gh";
-            this.lbl_gh.Size = new System.Drawing.Size(61, 16);
+            this.lbl_gh.Size = new System.Drawing.Size(74, 20);
             this.lbl_gh.TabIndex = 0;
             this.lbl_gh.Text = "Giỏ hàng";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(139, 299);
+            this.button1.Location = new System.Drawing.Point(156, 374);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(171, 23);
+            this.button1.Size = new System.Drawing.Size(192, 29);
             this.button1.TabIndex = 1;
             this.button1.Text = "Them vao gio";
             this.button1.UseVisualStyleBackColor = true;
@@ -55,22 +57,33 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(139, 234);
+            this.checkBox1.Location = new System.Drawing.Point(156, 292);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(60, 20);
+            this.checkBox1.Size = new System.Drawing.Size(73, 24);
             this.checkBox1.TabIndex = 2;
             this.checkBox1.Text = "Chọn";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(848, 86);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 62);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Đăng Nhâp";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.lbl_gh);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -83,6 +96,7 @@
         private System.Windows.Forms.Label lbl_gh;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
