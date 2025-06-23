be898021cf610807a28f90c73cbcfb346ed531ad
Ttam040402 <vothithanhtam.020404@gmail.com>
2025-06-23 10:29:22 +0700

    2280602853_VoThiThanhTam da them SP vao gio

--- WebTraSua/WebTraSua/Form1.Designer.cs
+++ WebTraSua/WebTraSua/Form1.Designer.cs
@@ -29,23 +29,48 @@
         private void InitializeComponent()
         {
             this.lbl_gh = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // lbl_gh
             // 
             this.lbl_gh.AutoSize = true;
-            this.lbl_gh.Location = new System.Drawing.Point(356, 78);
+            this.lbl_gh.Location = new System.Drawing.Point(189, 71);
+            this.lbl_gh.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_gh.Name = "lbl_gh";
-            this.lbl_gh.Size = new System.Drawing.Size(50, 13);
+            this.lbl_gh.Size = new System.Drawing.Size(61, 16);
             this.lbl_gh.TabIndex = 0;
             this.lbl_gh.Text = "Giỏ hàng";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(139, 299);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Them vao gio";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(139, 234);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(60, 20);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Chọn";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.lbl_gh);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -56,6 +81,8 @@
         #endregion
 
         private System.Windows.Forms.Label lbl_gh;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
